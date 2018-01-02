0cd8f1a3913f9ab6203f48562113a0f168e2f350
Dileep S <dileep92s@gmail.com>
2018-01-02 23:38:15 +0530

    lane detection with screen capture

--- src/lane_detect_scrcap.py
+++ src/lane_detect_scrcap.py
@@ -2,53 +2,43 @@
 import time
 import cv2
 import numpy as np
-from PIL import ImageGrab
+from grabscreen import grab_screen
 
-
-frame = np.array(ImageGrab.grab(bbox=(0,0,1024,728)))
-frame.reshape((1024,728,3))
 
 roi = None
-frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-roi = np.zeros(frame.shape, np.uint8)
-roi.reshape(frame.shape)
-height, width = frame.shape
-pts = np.array([[0.05*width, 0.90*height], [0.45*width, 0.6*height], [0.55*width, 0.6*height], [0.95*width, 0.90*height]], np.int32)
-cv2.fillPoly(roi, [pts], (255, 255, 255))
-iroi = cv2.bitwise_not(roi)
-
+iroi = None
 
 while 1:
-    frame = np.array(ImageGrab.grab(bbox=(0,0,1024,728)))
-    frame.reshape((1024,728,3))
-
+    frame = grab_screen(region=(40, 80, 640, 480))
+    # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR) 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    if roi is None:
+        roi = np.zeros(gray.shape, np.uint8)
+        height, width = gray.shape
+        pts = np.array([[0.05*width, 0.90*height], [0.45*width, 0.6*height], [0.55*width, 0.6*height], [0.95*width, 0.90*height]], np.int32)
+        cv2.fillPoly(roi, [pts], (255, 255, 255))
+        iroi = cv2.bitwise_not(roi)
+
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
     gray = cv2.Canny(gray, 100, 200)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
     gray = np.bitwise_and(gray, roi)
 
     frame1 = cv2.bitwise_and(frame,frame, mask=iroi)
-    
-    lines = cv2.HoughLines(gray,1,np.pi/180,30)
+    lines = cv2.HoughLinesP(gray,1,np.pi/180,100,np.array([]),100,50)
+    yy2 = 0
+    yy1 = 0
     if lines is not None:
-        ldone, rdone = False, False
-        for i in range(0, len(lines)):
-            rho = lines[i][0][0]
-            theta = lines[i][0][1]       
-
-            x1 = int(rho*np.cos(theta) + 1000*(-np.sin(theta)))
-            y1 = int(rho*np.sin(theta) + 1000*(np.cos(theta)))
-
-            x2 = int(rho*np.cos(theta) - 1000*(-np.sin(theta)))
-            y2 = int(rho*np.sin(theta) - 1000*(np.cos(theta)))
-            
-            if rho > 0 and not ldone:
-                cv2.line(frame,(x1,y1),(x2,y2),(255,0,255),4)    
-                ldone = True
-
-            if rho < 0 and not rdone:
-                cv2.line(frame,(x1,y1),(x2,y2),(255,0,255),4)    
-                rdone = True
+        for line in lines:
+            # print line
+            for x1,y1,x2,y2 in line:
+                if y1 not in range(yy1-200,yy1+200):
+                    cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),2)
+                    yy1 = (y1 + yy1)/2;
+                if y2 not in range(yy2-200,yy2+200):
+                    cv2.line(frame,(x1,y1),(x2,y2),(255,0,0),2)
+                    yy2 = (y2 + yy2)/2;
 
     
     frame2 = cv2.bitwise_or(frame, frame, mask=roi)
