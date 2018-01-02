b44169cec04c3b40d286978bf036162b8c380152
Dileep S <dileep92s@gmail.com>
2018-01-02 22:58:39 +0530

    optimize

--- src/lane_detect.py
+++ src/lane_detect.py
@@ -5,59 +5,56 @@ import numpy as np
 
 
 cap = cv2.VideoCapture(r'../data/solidYellowLeft.mp4')
-
-ret, frame = cap.read()
+# cap = cv2.VideoCapture(r'../data/challenge.mp4')
+# cap = cv2.VideoCapture(r'../data/solidWhiteRight.mp4')
 roi = None
-if ret:
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    roi = np.zeros(frame.shape, np.uint8)
-    roi.reshape(frame.shape)
-    height, width = frame.shape
-    pts = np.array([[0.05*width, 0.90*height], [0.45*width, 0.6*height], [0.55*width, 0.6*height], [0.95*width, 0.90*height]], np.int32)
-    cv2.fillPoly(roi, [pts], (255, 255, 255))
-    iroi = cv2.bitwise_not(roi)
-
+iroi = None
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
-
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (5, 5), 0)
-    gray = cv2.Canny(gray, 100, 200)
-    gray = np.bitwise_and(gray, roi)
 
-    frame1 = cv2.bitwise_and(frame,frame, mask=iroi)
-    
-    lines = cv2.HoughLines(gray,1,np.pi/180,30)
+    if roi is None:
+        roi = np.zeros(gray.shape, np.uint8)
+        height, width = gray.shape
+        pts = np.array([[0.05*width, 0.90*height], [0.45*width, 0.6*height],
+                        [0.55*width, 0.6*height],  [0.95*width, 0.90*height]], np.int32)
+        cv2.fillPoly(roi, [pts], (255, 255, 255))
+        iroi = cv2.bitwise_not(roi)
+
+    gray = cv2.Canny(gray, 100, 200)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    gray = np.bitwise_and(gray, roi)
+    frame1 = cv2.bitwise_and(frame, frame, mask=iroi)
+       
+    lines = cv2.HoughLines(gray, 1, np.pi/180, 30)
     if lines is not None:
         ldone, rdone = False, False
         for i in range(0, len(lines)):
             rho = lines[i][0][0]
-            theta = lines[i][0][1]       
+            theta = lines[i][0][1]
 
             x1 = int(rho*np.cos(theta) + 1000*(-np.sin(theta)))
             y1 = int(rho*np.sin(theta) + 1000*(np.cos(theta)))
 
             x2 = int(rho*np.cos(theta) - 1000*(-np.sin(theta)))
             y2 = int(rho*np.sin(theta) - 1000*(np.cos(theta)))
-            
+
             if rho > 0 and not ldone:
-                cv2.line(frame,(x1,y1),(x2,y2),(255,0,255),4)    
+                cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 255), 4)
                 ldone = True
 
             if rho < 0 and not rdone:
-                cv2.line(frame,(x1,y1),(x2,y2),(255,0,255),4)    
+                cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 255), 4)
                 rdone = True
 
-    
     frame2 = cv2.bitwise_or(frame, frame, mask=roi)
-    res = cv2.bitwise_or(frame1,frame2)
-
-    cv2.imshow("vidg",gray)
-    cv2.imshow("vid",res)
-    time.sleep(0.05)
+    res = cv2.bitwise_or(frame1, frame2)
+    cv2.imshow("vidg", gray)
+    cv2.imshow("vid", res)
+    # time.sleep(0.05)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
